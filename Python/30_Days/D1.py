b1d6ae07644d590f36a0d91c299ecf66fc9ce513
vybhav72954 <49750343+vybhav72954@users.noreply.github.com>
2020-06-16 16:24:49 +0530

    Create D1.py

---
+++ Python/30_Days/D1.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+a= input ()
+type (a)
+b= input ()
+# Read and save an integer, double, and String to your variables.
+c= input ()
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+print (i+int(a))
+print(d+ float(b))
+print(s +c)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
