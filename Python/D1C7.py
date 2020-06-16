410282ecc32e6eb16c3b8657653f4b0622289c47
vybhav72954 <49750343+vybhav72954@users.noreply.github.com>
2020-06-16 21:00:04 +0530

    Create D1C7.py

---
+++ Python/D1C7.py
@@ -0,0 +1,8 @@
+# Good List Comprehension
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    result = [[a, b, c] for a in range (x + 1) for b in range (y + 1) for c in range (z + 1) if ((a + b+ c) != n)]
+    print (result)
